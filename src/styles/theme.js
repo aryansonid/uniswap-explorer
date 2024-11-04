d71fe9f57a227fc245093a9e86009aec5a636e02
Aryan Soni <aryan.s400029@gmail.com>
2024-11-04 13:19:10 +0530

    Update: App theme chakra UI

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,8 +1,20 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  initialColorMode: "light",
+  initialColorMode: 'light',
   useSystemColorMode: false,
+  components: {
+    Table: {
+      variants: {
+        custom: {
+          td: {
+            border: '1px solid',
+            borderColor: 'gray.300',
+          },
+        },
+      },
+    },
+  },
 });
 
 export default theme;
