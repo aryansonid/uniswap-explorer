34e4e3a9e722f7b399c8d35606ccc6e3383a4bc9
Aryan Soni <aryan.s400029@gmail.com>
2024-11-04 13:18:38 +0530

    Update: TokenTable component for graph

--- src/components/TokenTable.js
+++ src/components/TokenTable.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+'use client';
+import React, { useState } from 'react';
 import {
   Box,
   Table,
@@ -10,15 +11,11 @@ import {
   Text,
   HStack,
   IconButton,
+  Spinner,
 } from '@chakra-ui/react';
-import {
-  FaSort,
-  FaSortUp,
-  FaSortDown,
-} from 'react-icons/fa';
-import axios from 'axios';
-import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
-import { format } from 'date-fns'; // Import the format function
+import { FaSort, FaSortUp, FaSortDown } from 'react-icons/fa';
+import useFetchTokenData from '../hooks/useFetchTokenData';
+import { LineChart, Line } from 'recharts';
 
 const headers = [
   { key: 'token', label: 'Token name' },
@@ -28,86 +25,13 @@ const headers = [
   { key: 'volumeChart', label: 'Volume Chart' },
 ];
 
-const query = `
-query MyQuery {
-  tokens {
-    symbol
-    volume
-    name
-    id
-    tokenDayData {
-      high
-      close
-      low
-      open
-      priceUSD
-      volume
-      volumeUSD
-      date
-    }
-    volumeUSD
-  }
-}
-`;
-
-const fetchSubgraphData = async (api, query) => {
-  try {
-    const response = await axios.post(api, { query });
-    return response.data.data.tokens;
-  } catch (error) {
-    console.error('Error fetching data from subgraph:', error);
-    throw error;
-  }
-};
-
 const TokenTable = () => {
-  const [tokenData, setTokenData] = useState([]);
+  const apiUrl = process.env.REACT_APP_SUBGRAPH_API_URL;
+  const { tokenData, loading } = useFetchTokenData(apiUrl);
   const [sortConfig, setSortConfig] = useState({
     key: 'token',
     direction: 'ascending',
   });
-
-  useEffect(() => {
-    const fetchAndProcessData = async () => {
-      try {
-        const data = await fetchSubgraphData(
-          process.env.REACT_APP_SUBGRAPH_API_URL,
-          query
-        );
-        const processedData = data.map(token => {
-          const { tokenDayData } = token;
-          const lastDayData = tokenDayData[tokenDayData.length - 1];
-
-          if (lastDayData) {
-            const open = parseFloat(lastDayData.open);
-            const close = parseFloat(lastDayData.close);
-            const percentageChange = !isNaN(open) && !isNaN(close)
-              ? ((close - open) / open) * 100
-              : 'N/A';
-
-            return {
-              ...token,
-              percentageChange: percentageChange.toFixed(2),
-              tokenDayData: tokenDayData.map(day => ({
-                date: format(new Date(day.date * 1000), 'yyyy-MM-dd'), // Convert to readable format
-                priceUSD: parseFloat(day.priceUSD) || 0,
-                volume: parseFloat(day.volume) || 0,
-              })),
-            };
-          }
-          return {
-            ...token,
-            percentageChange: 'N/A',
-          };
-        });
-        setTokenData(processedData);
-      } catch (error) {
-        console.error('Error in fetchAndProcessData:', error);
-      }
-    };
-
-    fetchAndProcessData();
-  }, []);
 
   const sortedTokenData = [...tokenData].sort((a, b) => {
     const aValue =
@@ -141,65 +65,90 @@ const TokenTable = () => {
       <Text fontSize="2xl" mb={4}>
         Tokens
       </Text>
-      <Table>
-        <Thead bg="#f9f9f9" color="#7d7d7d">
-          <Tr>
-            {headers.map(header => (
-              <Th key={header.key} textTransform="none">
-                <HStack>
-                  <Text>{header.label}</Text>
-                  <IconButton
-                    icon={
-                      sortConfig.key === header.key &&
-                      sortConfig.direction === 'ascending' ? (
-                        <FaSortUp />
-                      ) : sortConfig.key === header.key &&
-                        sortConfig.direction === 'descending' ? (
-                        <FaSortDown />
-                      ) : (
-                        <FaSort />
-                      )
-                    }
-                    variant="link"
-                    onClick={() => requestSort(header.key)}
-                    size="sm"
-                    aria-label={`Sort by ${header.label}`}
-                  />
-                </HStack>
-              </Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody>
-          {sortedTokenData.map((token, index) => (
-            <Tr key={index}>
+      {loading ? (
+        <HStack justify="center" mt={4}>
+          <Spinner size="xl" />
+        </HStack>
+      ) : (
+        <Table variant="simple">
+          <Thead bg="#f9f9f9" color="#7d7d7d">
+            <Tr>
               {headers.map(header => (
-                <Td key={header.key}>
-                  {header.key === 'token' ? (
-                    <HStack spacing={2}>
-                      <Text>{token.symbol}</Text>
-                    </HStack>
-                  ) : header.key === '1d_change' ? (
-                    `${token.percentageChange}%`
-                  ) : header.key === 'volumeChart' ? (
-                    <Box width={200} height={100}>
-                      <LineChart width={200} height={100} data={token.tokenDayData}>
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="date" />
-                        <YAxis />
-                        <Tooltip />
-                        <Line type="monotone" dataKey="volume" stroke="#82ca9d" />
-                      </LineChart>
-                    </Box>
-                  ) : (
-                    token[header.key] || 'N/A'
-                  )}
-                </Td>
+                <Th key={header.key} textTransform="none" width="64px">
+                  <HStack>
+                    <Text
+                      onClick={() =>
+                        header.key === 'token' && requestSort(header.key)
+                      }
+                      cursor="pointer"
+                    >
+                      {header.label}
+                    </Text>
+                    <IconButton
+                      icon={
+                        sortConfig.key === header.key &&
+                        sortConfig.direction === 'ascending' ? (
+                          <FaSortUp />
+                        ) : sortConfig.key === header.key &&
+                          sortConfig.direction === 'descending' ? (
+                          <FaSortDown />
+                        ) : (
+                          <FaSort />
+                        )
+                      }
+                      variant="link"
+                      onClick={() => requestSort(header.key)}
+                      size="sm"
+                      aria-label={`Sort by ${header.label}`}
+                    />
+                  </HStack>
+                </Th>
               ))}
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
+          </Thead>
+          <Tbody>
+            {sortedTokenData.map((token, index) => (
+              <Tr key={index} height="64px">
+                {headers.map(header => (
+                  <Td
+                    key={header.key}
+                    width="64px"
+                    height="64px"
+                    verticalAlign="middle"
+                  >
+                    {header.key === 'token' ? (
+                      <HStack spacing={2}>
+                        <Text>{token.symbol}</Text>
+                      </HStack>
+                    ) : header.key === '1d_change' ? (
+                      `${token.percentageChange}%`
+                    ) : header.key === 'volumeChart' ? (
+                      <Box width={200} height={100}>
+                        <LineChart
+                          width={200}
+                          height={100}
+                          data={token.tokenDayData}
+                          margin={{ top: 5, right: 30, left: 0, bottom: 5 }}
+                        >
+                          <Line
+                            type="monotone"
+                            dataKey="volume"
+                            stroke="#82ca9d"
+                            strokeWidth={2}
+                            dot={false}
+                          />
+                        </LineChart>
+                      </Box>
+                    ) : (
+                      (token[header.key] || 0).toString()
+                    )}
+                  </Td>
+                ))}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      )}
     </Box>
   );
 };
