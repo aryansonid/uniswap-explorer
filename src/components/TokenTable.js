57fe92276a8eed367ca9701c8157a3de295319ec
Aryan Soni <aryan.s400029@gmail.com>
2024-11-04 15:19:39 +0530

    Refactor: Added Table border, fontSize, padding and item avatar

--- src/components/TokenTable.js
+++ src/components/TokenTable.js
@@ -11,13 +11,15 @@ import {
   Text,
   HStack,
   IconButton,
-  Spinner,
+  Avatar,
 } from '@chakra-ui/react';
 import { FaSort, FaSortUp, FaSortDown } from 'react-icons/fa';
 import useFetchTokenData from '../hooks/useFetchTokenData';
 import { LineChart, Line } from 'recharts';
+import TableSkeleton from './TableSkeleton';
 
 const headers = [
+  { key: 'index', label: '#' },
   { key: 'token', label: 'Token name' },
   { key: 'price', label: 'Price' },
   { key: '1d_change', label: '1 day' },
@@ -61,93 +63,114 @@ const TokenTable = () => {
   };
 
   return (
-    <Box p={2}>
+    <Box px={16}>
       <Text fontSize="2xl" mb={4}>
         Tokens
       </Text>
       {loading ? (
         <HStack justify="center" mt={4}>
-          <Spinner size="xl" />
+          <TableSkeleton />
         </HStack>
       ) : (
-        <Table variant="simple">
-          <Thead bg="#f9f9f9" color="#7d7d7d">
-            <Tr>
-              {headers.map(header => (
-                <Th key={header.key} textTransform="none" width="64px">
-                  <HStack>
-                    <Text
-                      onClick={() =>
-                        header.key === 'token' && requestSort(header.key)
-                      }
-                      cursor="pointer"
-                    >
-                      {header.label}
-                    </Text>
-                    <IconButton
-                      icon={
-                        sortConfig.key === header.key &&
-                        sortConfig.direction === 'ascending' ? (
-                          <FaSortUp />
-                        ) : sortConfig.key === header.key &&
-                          sortConfig.direction === 'descending' ? (
-                          <FaSortDown />
-                        ) : (
-                          <FaSort />
-                        )
-                      }
-                      variant="link"
-                      onClick={() => requestSort(header.key)}
-                      size="sm"
-                      aria-label={`Sort by ${header.label}`}
-                    />
-                  </HStack>
-                </Th>
-              ))}
-            </Tr>
-          </Thead>
-          <Tbody>
-            {sortedTokenData.map((token, index) => (
-              <Tr key={index} height="64px">
+        <Box
+          mt={4}
+          borderRadius="3xl"
+          overflow="hidden"
+          border="1px"
+          borderColor="gray.100"
+        >
+          <Table variant="simple" borderRadius="xl">
+            <Thead>
+              <Tr>
                 {headers.map(header => (
-                  <Td
+                  <Th
                     key={header.key}
-                    width="64px"
-                    height="64px"
-                    verticalAlign="middle"
+                    textTransform="none"
+                    height="48px"
+                    bg={'#f9f9f9'}
+                    color={'#7d7d7d'}
                   >
-                    {header.key === 'token' ? (
-                      <HStack spacing={2}>
-                        <Text>{token.symbol}</Text>
-                      </HStack>
-                    ) : header.key === '1d_change' ? (
-                      `${token.percentageChange}%`
-                    ) : header.key === 'volumeChart' ? (
-                      <Box width={200} height={100}>
-                        <LineChart
-                          width={200}
-                          height={100}
-                          data={token.tokenDayData}
-                          margin={{ top: 5, right: 30, left: 0, bottom: 5 }}
-                        >
-                          <Line
-                            type="monotone"
-                            dataKey="volume"
-                            stroke="#82ca9d"
-                            strokeWidth={2}
-                            dot={false}
-                          />
-                        </LineChart>
-                      </Box>
-                    ) : (
-                      (token[header.key] || 0).toString()
-                    )}
-                  </Td>
+                    <HStack>
+                      <Text
+                        fontSize={'16px'}
+                        onClick={() =>
+                          header.key !== 'index' && requestSort(header.key)
+                        }
+                        cursor={header.key !== 'index' ? 'pointer' : 'default'}
+                      >
+                        {header.label}
+                      </Text>
+                      <IconButton
+                        icon={
+                          sortConfig.key === header.key &&
+                          sortConfig.direction === 'ascending' ? (
+                            <FaSortUp />
+                          ) : sortConfig.key === header.key &&
+                            sortConfig.direction === 'descending' ? (
+                            <FaSortDown />
+                          ) : (
+                            <FaSort />
+                          )
+                        }
+                        variant="link"
+                        onClick={() =>
+                          header.key !== 'index' && requestSort(header.key)
+                        }
+                        size="sm"
+                        aria-label={`Sort by ${header.label}`}
+                      />
+                    </HStack>
+                  </Th>
                 ))}
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
+            </Thead>
+            <Tbody>
+              {sortedTokenData.map((token, index) => (
+                <Tr key={index} height="64px">
+                  {headers.map(header => (
+                    <Td key={header.key} height="64px" verticalAlign="middle">
+                      {header.key === 'index' ? (
+                        <Text fontSize="16px">{index + 1}</Text>
+                      ) : header.key === 'token' ? (
+                        <HStack spacing={2}>
+                          <Avatar
+                            size="sm"
+                            src={`/logo/${token.symbol.toLowerCase()}.png`}
+                            alt={token.symbol}
+                          />
+                          <Text fontSize="16px">{token.symbol}</Text>
+                        </HStack>
+                      ) : header.key === '1d_change' ? (
+                        <Text fontSize="16px">{`${token.percentageChange}%`}</Text>
+                      ) : header.key === 'volumeChart' ? (
+                        <Box width={200} overflow="hidden">
+                          <LineChart
+                            width={200}
+                            height={32}
+                            data={token.tokenDayData}
+                            margin={{ top: 5, right: 30, left: 0, bottom: 5 }}
+                          >
+                            <Line
+                              type="monotone"
+                              dataKey="volume"
+                              stroke="#82ca9d"
+                              strokeWidth={2}
+                              dot={false}
+                            />
+                          </LineChart>
+                        </Box>
+                      ) : (
+                        <Text fontSize="16px">
+                          {(token[header.key] || 0).toString()}
+                        </Text>
+                      )}
+                    </Td>
+                  ))}
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
       )}
     </Box>
   );
